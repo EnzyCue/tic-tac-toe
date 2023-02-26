50e0dc289c00714211fb5538676427a4e58e2f7a
EnzyCue <mohammadalhakim787@gmail.com>
2023-02-26 13:04:03 +1100

    Add basic Run function and board interaction functions.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,60 @@
 
+const player = (name, symbol) => {
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    return {getName, getSymbol};
+}
+
 const gameBoard = (() => {
-    let this.gameBoard = [];
+    let gameBoard = ['?', '?', '?', '?', '?', '?', '?', '?', '?'];
+    return gameBoard;
 })();
 
-const player = (name) => {
-    this.name = name;
+function boardInteract(player, button){
+    button.textContent = player.getSymbol();
+    // button.disbled = "disabled";
 }
 
-const displayTurn = (turn) => {
-    this.turn = turn;
+function render(board) {
+    for(let i = 0; i < 9; ++i){
+        let boardButton = document.querySelector(`.board-button[data-index="${i}"]`);
+        boardButton.textContent =  board[i];
+    }
 }
+
+function run() {
+    const player1 = player('player1', 'X');
+    const player2 = player('player2', 'O');
+
+    let currentPlayer = player1;
+
+    render(gameBoard);
+    buttonSetup();
+
+    function buttonSetup(){
+        let boardButtonsAll = document.querySelectorAll('.board-button');
+        boardButtonsAll.forEach(boardButton => {
+            boardButton.addEventListener('click', () => {
+                boardInteract(currentPlayer, boardButton);
+                currentPlayer = (currentPlayer === player1) ? player2 : player1;
+            });
+
+        });
+
+    };
+};
+
+run();
+
+
+
+// const exampleFactor = (name) =>{
+//     const object = {name};
+//     return object;
+// }
+
+// const exampleInheritor = () => {
+//     const {juicer} = exampleFactor('yeet');
+//     const bruh = () => console.log('bruh we inherited smthn fr fr.')
+//     return {juicer, bruh};
+// }
