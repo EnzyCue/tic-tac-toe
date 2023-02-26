c5ae6bf51dda7b9a6a8b91d4374434ed0e89a716
EnzyCue <mohammadalhakim787@gmail.com>
2023-02-26 11:25:48 +1100

    Setup basic html and css with gameboard buttons and site header.

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+const gameBoard = (() => {
+    let this.gameBoard = [];
+})();
+
+const player = (name) => {
+    this.name = name;
+}
+
+const displayTurn = (turn) => {
+    this.turn = turn;
+}
